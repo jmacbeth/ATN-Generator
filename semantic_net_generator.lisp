952d760fe172519912d6c1324278f8d4513544bb
Rebecca Runxin Wang <33033191+RebeccaRunxinWang@users.noreply.github.com>
2019-04-04 01:43:59 -0400

    Update semantic_net_generator.lisp

--- semantic_net_generator.lisp
+++ semantic_net_generator.lisp
@@ -1,30 +1,32 @@
-;;This code generates a semantic network using Conceptual Dependency structure
-;;It can only deal with atrans right now. 
-;;Now it works for generating once. Still have bug matching g-nodes when it generates from multiple cds and frameworks
+;; This code generates a semantic network using Conceptual Dependency structure
+;; It can only deal with atrans right now. 
+;; Now it works for generating once. Still have bug matching g-nodes when it generates from multiple cds and frameworks
+;; Changed the format of the "framework" expression to make code more elegant
 
 (defun generate (cd frame) 
-  (newsym 'g)
-  (newsym 'g)
-  (newsym 'g) 
+  (setq agtsym (newsym 'g)) ;;g0
+  (setq objsym (newsym 'g)) ;;g1
+  (setq lexptrsym (newsym 'g)) ;;g2
   (mapcar  #'(lambda (x)  
   (case (car x)
    ((agt)
-   (append `(g1 (tok ,(caadr (assoc (cadr x) (cdr cd))) nbr sing det none pron none agt* (g0) ns ())) ))
+   (append `(agtsym (tok ,(caadr (assoc (cadr x) (cdr cd))) nbr sing det none pron none agt* (,lexptrsym) ns ())) ))
    ((obj) 
-   (append `(g2 (tok ,(caadr (assoc (cadr x) (cdr cd))) det indef nbr sing pron none obj* (g0) ns ())) ))
+   (append `(objsym (tok ,(caadr (assoc (cadr x) (cdr cd))) det indef nbr sing pron none obj* (,lexptrsym) ns ())) ))
    ((lexptr) ;; used mapcar to deal with the verb as well
-   (append `(g0 (tok ,(second frame) voice active form none aspect none auxverb none tense past mood indic aux none time 1 pron none agt (g1) obj (g2) vs () )))
+   (append `(lexptrsym (tok ,(second frame) voice active form none aspect none auxverb none tense past mood indic aux none time 1 pron none agt (,agtsym) obj (,objsym) vs () )))
  )
  ))
-;; added (lexptr l-node) pair to the alist, in order to use mapcar to create the node for atrans along with other nodes
-  (push `(,(car frame) ,(second frame)) (car (last frame))) 
-)) 
+  frame
+))
+
  
 (defparameter *cd1* '(atrans (actor (l-john)) (object (l-book))))
-(defparameter *frame1* '(lexptr l-give framework ((agt actor) (obj object))))
+;; changed the format of the "framework" expression to make code more elegant
+(defparameter *frame1* '((agt actor) (obj object) (lexptr l-give)))
 
 (defparameter *cd2* '(atrans (actor (l-mary)) (object (l-beer))))
-(defparameter *frame2* '(lexptr l-drink framework ((agt actor) (obj object))))
+(defparameter *frame2* '((agt actor) (obj object) (lexptr l-drink)))
 
 
 (defun newsym (sym)
@@ -33,8 +35,6 @@
     (if (null count) (setf count 0))
     (setf (get sym 'usage-count) (1+ count))
     (intern (concatenate 'string (string sym)
-			 (prin1-to-string count)))))
+       (prin1-to-string count)))))
 
-(print(generate *cd1* *frame1*))
-(print(generate *cd2* *frame2*))
-
+(generate *cd1* *frame1*)
